da71c2f955a4b4f44ff5f24bc728c9cf5a3a8703
曾烟然 <1020444587@qq.com>
2020-01-01 00:01:44 +0800

    上传scr文件

---
+++ src/main.js
@@ -0,0 +1,13 @@
+import Vue from 'vue'
+import App from './App'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css';
+import router from './router'
+Vue.config.productionTip = false
+Vue.use(ElementUI);
+ new Vue({
+     el: '#app',
+     router,
+     components:{ App },
+     template:'<App/>'
+})
---
+++ src/router/index.js
@@ -0,0 +1,42 @@
+import Vue from 'vue'
+import '@/assets/css/mui.min.css'
+import '@/assets/css/app.css'
+import Router from 'vue-router'
+import HelloWorld from '@/components/HelloWorld'
+import card from '@/components/card'
+import nav from '@/components/nav'
+import home from '@/components/home'
+import index from '@/components/index'
+
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/HelloWorld',
+      name: 'HelloWorld',
+      component: HelloWorld
+    },
+	{
+	  path: '/card',
+	  name: 'card',
+	  component: card
+	},
+  {
+    path: '/',
+    name: 'nav',
+    component: nav
+  },
+  {
+    path: '/home',
+    name: 'home',
+    component: home
+  },
+  {
+    path: '/index',
+    name: 'index',
+    component: index
+  }
+	 ]
+})
